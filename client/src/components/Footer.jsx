cd1df248700c3838af0af63ebd17d032438dd709
Divyansh Bansal <bansal9divyansh@gmail.com>
2023-04-29 10:46:57 +0530

    Add Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container flex footer-container">
+        <p>
+          <a href="#" className="company-logo">
+            <img src="/assets/asset 1.png" alt="company logo" />
+          </a>
+          was born from the visionary idea of Divyansh Bansal, continually evolving to meet the needs of our users. We're driven by a commitment to safety and empowerment, revolutionizing transportation with each update.
+          With cutting-edge technology and community feedback, we're reshaping the future of safe travel, one ride at a time.
+          Join us on our journey towards a safer, more empowered world.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
