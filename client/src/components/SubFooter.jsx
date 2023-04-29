464af3185be6ccd39fcc18d9044b16f8317b0780
Divyansh Bansal <bansal9divyansh@gmail.com>
2023-04-29 11:21:36 +0530

    Add SubFooter component

---
+++ client/src/components/SubFooter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SubFooter = () => {
+  return (
+    <div className="subfooter">
+      <div className="container flex subfooter-container">
+        <a className="hover-link" href="#">Privacy policy</a>
+        <a className="hover-link" href="#">Terms & Condition</a>
+        <a className="hover-link" href="#">Security Information</a>
+        <a className="hover-link" href="#"><i className="fa-brands fa-instagram"></i></a>
+        <a className="hover-link" href="#"><i className="fa-brands fa-x"></i></a>
+      </div>
+    </div>
+  );
+};
+
+export default SubFooter;
